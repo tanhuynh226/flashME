7387ccb9822c3aa7878757e04d4b8034ce64216e
tanhuynh226 <tanhuynh226@yahoo.com>
2021-01-31 09:40:14 -0800

    Preparation for deployment

---
+++ secret.py
@@ -0,0 +1,5 @@
+# secret.py in .gitignore
+
+db_uri = "mongodb+srv://chris:zumaJK8kuFPJlN7S@cluster0.yp3zl.mongodb.net/flashDB?retryWrites=true&w=majority"
+google_client_id = "591191830884-iesl69sb9n8hukk9jldkhjjbdmgf2hpi.apps.googleusercontent.com"
+google_client_secret = "DwwVbC_7zBSHLB-_coC0bHbo"
